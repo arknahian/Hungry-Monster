b5fc891d95997d38d4cda1cbaf5c2bbed3d79dc3
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2021-10-13 08:31:25 +0600

    fetch and other stuff

---
+++ script.js
@@ -0,0 +1,65 @@
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", () => {
+    const userInput = document.querySelector("#user-input");
+    getData(userInput)
+    .then(res => {
+        console.log(res)
+        console.log(res.meals)
+
+        const meals = res.meals;
+        meals.forEach(item => {
+            console.log(item.strMeal);
+            
+            const foodCardContainer = document.getElementById("food-card-container");
+
+            const newFoodCard = document.createElement("div");
+            newFoodCard.className = "food-card";
+
+            newFoodCard.addEventListener("click", (event) => {
+                console.log(event.target.parentElement.childNodes[0].src);
+                // const targetImg = event.target.parentNode.img.src;
+                const popFoodName = document.querySelector("#popup-food-name");
+                const popup = document.querySelector("#popup");
+                popup.style.display = "flex";
+                popFoodName.innerText = event.target.parentElement.childNodes[1].innerText;
+                const popupImg = document.querySelector("#popup-img");
+                popupImg.src = event.target.parentElement.childNodes[0].src;
+
+                const crossBtn = document.querySelector("#cross")
+                crossBtn.addEventListener("click", () => {
+                    popup.style.display = "none";
+                })
+            })
+
+
+
+
+            const newFoodImg = document.createElement("img");
+            newFoodImg.className = "food-img";
+            newFoodImg.src = item.strMealThumb;
+            newFoodCard.appendChild(newFoodImg);
+
+            const newFoodName = document.createElement("h3");
+            newFoodName.className = "food-name";
+            newFoodName.innerText = item.strMeal;
+            newFoodCard.appendChild(newFoodName);
+
+            foodCardContainer.appendChild(newFoodCard);
+
+        })
+    })
+    .catch(err => {
+        alert("fuck you")
+    })
+})
+
+const getData = async (input) => {
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${input.value}`);
+    if (!res.ok) {
+        const message = `Error ${res.status}`
+        throw new Error(message);
+    }
+    const data = await res.json();
+    return data;
+}
+
